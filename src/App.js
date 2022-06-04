dc22ec1337d183847d9ed7c82006e34f2b44b8a5
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-06-04 12:57:35 +0600

    chart added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyLineChart from "./Components/MyLineChart/MyLineChart";
 
 function App() {
   return (
     <div className="App">
-    
+      <MyLineChart></MyLineChart>
     </div>
   );
 }
